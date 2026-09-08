7bb46419d5f497b4a60092bd375b8acb43d53e55
Sergei Mikurov <454004@mail.ru>
2026-09-08 23:25:07 +0500

    test(infra): Add test producer sent message

--- tests/infrastructure/test_producer.py
+++ tests/infrastructure/test_producer.py
@@ -1,12 +1,38 @@
 """Test Producer."""
 
+from unittest.mock import Mock
+
+import pytest
+
 from eip.application.messages import Command
 from eip.infrastructure.messaging.abstract import (
+    AbstractChannel,
     AbstractProducer,
 )
+from eip.infrastructure.messaging.points import Producer
 
 
-def test_simple_sync_producer(
+@pytest.fixture
+def mock_channel() -> Mock:
+    """Provide channel mock."""
+    return Mock(spec=AbstractChannel)
+
+
+@pytest.fixture
+def producer(
+    mock_channel: AbstractChannel[Command],
+) -> AbstractProducer[Command]:
+    """Provide Producer."""
+    return Producer(channel=mock_channel)
+
+
+def test_producer_sent_message_to_channel(
+    command: Command,
+    mock_channel: Mock,
     producer: AbstractProducer[Command],
 ) -> None:
-    pass
+    # Act
+    producer.send(command)
+
+    # Assert
+    mock_channel.send.assert_called_once_with(command)
