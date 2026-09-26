75060ff6f52bf60bd103c0ffb3afedb4bba162e4
Sergei Mikurov <454004@mail.ru>
2026-09-26 20:40:03 +0500

    chore(make): Update commands

--- mk/examples.mk
+++ mk/examples.mk
@@ -11,6 +11,12 @@ run-example-socket-echo-server:
 run-example-socket-echo-client:
 	poetry run python3 src/eip/examples/socket/echo_client.py
 
+run-example-socket-multiconn-server:  ## Run socket multi-connect server example with args
+	poetry run python3 src/eip/examples/socket/multiconn_server.py 127.0.0.1 65432
+
+run-example-socket-multiconn-client:  ## Run socket multi-connect client example with args
+	poetry run python3 src/eip/examples/socket/multiconn_client.py 127.0.0.1 65432 2
+
 
 # Help
 # ====
@@ -23,6 +29,10 @@ help-examples:
 	@echo ""
 	@echo "Socket commands"
 	@echo "==============="
+	@echo ""
 	@echo "run-example-socket-echo-server		Run socket echo server example"
 	@echo "run-example-socket-echo-client		Run socket echo client example"
+	@echo ""
+	@echo "run-example-socket-multiconn-server	Run socket multi-connect server example with args"
+	@echo "run-example-socket-multiconn-client 	Run socket multi-connect client example with args"
 	@echo ""
--- mk/infrastructure.mk
+++ mk/infrastructure.mk
@@ -5,18 +5,6 @@
 # Socket commands
 # ===============
 
-run-socket-server:  ## Run socket server example
-	poetry run python3 src/eip/infrastructure/endpoints/server.py
-
-run-socket-client:  ## Run socket client example
-	poetry run python3 src/eip/infrastructure/endpoints/client.py
-
-run-socket-multiconn-server:  ## Run socket multi-connect server example with args
-	poetry run python3 src/eip/infrastructure/endpoints/multiconn_server.py 127.0.0.1 65432
-
-run-socket-multiconn-client:  ## Run socket multi-connect client example with args
-	poetry run python3 src/eip/infrastructure/endpoints/multiconn_client.py 127.0.0.1 65432 2
-
 run-socket-app-server:  # Run application socket server with args
 	poetry run python3 src/eip/infrastructure/endpoints/app_server.py '' 65432
 
@@ -46,10 +34,7 @@ help-infra:
 	@echo ""
 	@echo "Socket commands"
 	@echo "==============="
-	@echo "run-socket-server		Run socket server example"
-	@echo "run-socket-client		Run socket client example"
-	@echo "run-socket-multiconn-server	Run socket multi-connect server example (with args)"
-	@echo "run-socket-multiconn-client	Run socket multi-connect client example (with args)"
+	@echo ""
 	@echo "run-socket-app-server		Run application socket server (with args)"
 	@echo "run-socket-app-client		Run application socket client (with args)"
 	@echo ""
