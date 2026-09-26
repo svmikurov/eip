cb6deb2cc0831a269f5de8568fc5aaed10615152
Sergei Mikurov <454004@mail.ru>
2026-09-26 21:10:35 +0500

    chore(example): Remove multi-connection sockets

--- mk/examples.mk
+++ mk/examples.mk
@@ -5,12 +5,6 @@
 # Socket commands
 # ===============
 
-run-example-socket-echo-server:
-	poetry run python3 src/eip/examples/socket/echo_server.py
-
-run-example-socket-echo-client:
-	poetry run python3 src/eip/examples/socket/echo_client.py
-
 run-example-socket-multiconn-server:  ## Run socket multi-connect server example with args
 	poetry run python3 src/eip/examples/socket/multiconn_server.py 127.0.0.1 65432
 
@@ -29,9 +23,6 @@ help-examples:
 	@echo ""
 	@echo "Socket commands"
 	@echo "==============="
-	@echo ""
-	@echo "run-example-socket-echo-server		Run socket echo server example"
-	@echo "run-example-socket-echo-client		Run socket echo client example"
 	@echo ""
 	@echo "run-example-socket-multiconn-server	Run socket multi-connect server example with args"
 	@echo "run-example-socket-multiconn-client 	Run socket multi-connect client example with args"
